190a62978768b26ab0d2638e3e8e942d1084c550
shalu-007 <sunindus.sk@gmail.com>
2019-02-27 11:54:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/multiselecterrecyclerapp/MultiSelectableAdapter.java
@@ -0,0 +1,47 @@
+package com.example.multiselecterrecyclerapp;
+
+
+import android.support.v7.widget.RecyclerView;
+import android.util.SparseBooleanArray;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract  class MultiSelectableAdapter<VH extends RecyclerView.ViewHolder> extends RecyclerView.Adapter<VH> {
+
+   private SparseBooleanArray mSelectedItems;
+   public MultiSelectableAdapter(){
+       mSelectedItems=new SparseBooleanArray();
+   }
+   public  boolean isSelected(int position){
+       return  getSelectedItems().contains(position);
+   }
+    public int getSelectedItemCount() {
+        return mSelectedItems.size();
+    }
+
+    public List<Integer> getSelectedItems(){
+       List<Integer> items=new ArrayList<Integer>(mSelectedItems.size());
+      for(int i=0;i<mSelectedItems.size();i++){
+          items.add(mSelectedItems.keyAt(i));
+      }
+      return items;
+   }
+   public void  clearSelection(){
+       List<Integer> items=getSelectedItems();
+       mSelectedItems.clear();
+       for (Integer i:items){
+           notifyItemChanged(i);
+       }
+   }
+   public  void toggleSelection(int position){
+       if(mSelectedItems.get(position,false)){
+           mSelectedItems.delete(position);
+       }
+       else{
+           mSelectedItems.put(position,true);
+       }
+       notifyItemChanged(position);
+   }
+
+}
---
+++ app/src/main/java/com/example/multiselecterrecyclerapp/TextAdapter.java
@@ -0,0 +1,5 @@
+package com.example.multiselecterrecyclerapp;
+
+public class TextAdapter  {
+    
+}
